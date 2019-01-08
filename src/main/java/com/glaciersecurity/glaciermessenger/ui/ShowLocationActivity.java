84223d7047a57fd6fd34e80128895bd44a348c21
Christina Giampalmo <cgiampalmo@glaciersecurity.com>
2019-01-08 16:49:07 -0500

    quick fixes for AM_120, more to go

--- src/main/java/com/glaciersecurity/glaciermessenger/ui/ShowLocationActivity.java
+++ src/main/java/com/glaciersecurity/glaciermessenger/ui/ShowLocationActivity.java
@@ -165,27 +165,26 @@ public class ShowLocationActivity extends LocationActivity implements LocationLi
 	@Override
 	public boolean onOptionsItemSelected(final MenuItem item) {
 		switch (item.getItemId()) {
-//			case R.id.action_copy_location:
-//				final ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-//				if (clipboard != null) {
-//					final ClipData clip = ClipData.newPlainText("location", createGeoUri().toString());
-//					clipboard.setPrimaryClip(clip);
-//					Toast.makeText(this,R.string.url_copied_to_clipboard,Toast.LENGTH_SHORT).show();
-//				}
-//				return true;
-				//HONEYBADGER AM-120 Disable and hide "Share Location Plugin"
-//			case R.id.action_share_location:
-//				final Intent shareIntent = new Intent();
-//				shareIntent.setAction(Intent.ACTION_SEND);
-//				shareIntent.putExtra(Intent.EXTRA_TEXT, createGeoUri().toString());
-//				shareIntent.setType("text/plain");
-//				try {
-//					startActivity(Intent.createChooser(shareIntent, getText(R.string.share_with)));
-//				} catch (final ActivityNotFoundException e) {
-//					//This should happen only on faulty androids because normally chooser is always available
-//					Toast.makeText(this, R.string.no_application_found_to_open_file, Toast.LENGTH_SHORT).show();
-//				}
-//				return true;
+			case R.id.action_copy_location:
+				final ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+				if (clipboard != null) {
+					final ClipData clip = ClipData.newPlainText("location", createGeoUri().toString());
+					clipboard.setPrimaryClip(clip);
+					Toast.makeText(this,R.string.url_copied_to_clipboard,Toast.LENGTH_SHORT).show();
+				}
+				return true;
+			case R.id.action_share_location:
+				final Intent shareIntent = new Intent();
+				shareIntent.setAction(Intent.ACTION_SEND);
+				shareIntent.putExtra(Intent.EXTRA_TEXT, createGeoUri().toString());
+				shareIntent.setType("text/plain");
+				try {
+					startActivity(Intent.createChooser(shareIntent, getText(R.string.share_with)));
+				} catch (final ActivityNotFoundException e) {
+					//This should happen only on faulty androids because normally chooser is always available
+					Toast.makeText(this, R.string.no_application_found_to_open_file, Toast.LENGTH_SHORT).show();
+				}
+				return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
