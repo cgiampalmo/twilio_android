656f9f0ef0ae64afe6b911ecb1611b23a42cf7c1
Christina Giampalmo <cgiampalmo@glaciersecurity.com>
2020-07-29 11:37:22 -0400

    AM-467

--- src/main/java/com/glaciersecurity/glaciermessenger/ui/EditAccountActivity.java
+++ src/main/java/com/glaciersecurity/glaciermessenger/ui/EditAccountActivity.java
@@ -2383,7 +2383,8 @@ public class EditAccountActivity extends OmemoActivity implements OnAccountUpdat
 
 			} else {
 				closeWaitDialog();
-				showDialogMessage(getString(R.string.signin_fail_title), getString(R.string.login_error_message));
+				//
+				// showDialogMessage(getString(R.string.signin_fail_title), getString(R.string.login_error_message));
 
 				//ALF AM-143 log out of cognito
 				logOut();
