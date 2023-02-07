ce7a3ac3e75443d65ef08262d210544495a3b955
Christina Giampalmo <cgiampalmo@glaciersecurity.com>
2023-02-07 09:46:23 -0500

    start adding ui

---
+++ src/compat/java/com/glaciersecurity/glaciermessenger/ui/DialerActivity.java
@@ -0,0 +1,17 @@
+package com.glaciersecurity.glaciermessenger.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.glaciersecurity.glaciermessenger.R;
+import com.glaciersecurity.glaciermessenger.utils.ThemeHelper;
+
+public class DialerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialer);
+    }
+}
